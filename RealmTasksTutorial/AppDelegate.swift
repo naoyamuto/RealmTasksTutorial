9a3dff6fb41179959f966d77f03414e171b041b3
naoyamuto <naoya.mutou@gmail.com>
2017-03-05 22:56:26 +0900

    Set up

---
+++ RealmTasksTutorial/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  RealmTasksTutorial
+//
+//  Created by Naoya Muto on 2017/03/05.
+//  Copyright © 2017年 Naoya Muto. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:[UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController(style: .plain))
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ RealmTasksTutorial/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  RealmTasksTutorial
+//
+//  Created by Naoya Muto on 2017/03/05.
+//  Copyright © 2017年 Naoya Muto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+}
