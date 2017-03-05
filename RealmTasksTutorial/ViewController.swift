378817bc4ea0741def7cfd20cc476427e69086f4
naoyamuto <naoya.mutou@gmail.com>
2017-03-06 00:09:56 +0900

    Implement some methods

--- RealmTasksTutorial/ViewController.swift
+++ RealmTasksTutorial/ViewController.swift
@@ -10,14 +10,49 @@ import UIKit
 import RealmSwift
 
 class ViewController: UITableViewController {
+    var items = List<Task>()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
     }
-    
+
     func setupUI() {
         title = "My Tasks"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(add))
+    }
+
+    // MARK: Functions
+
+    func add() {
+        let alertController = UIAlertController(title: "New Task", message: "Enter Task Name", preferredStyle: .alert)
+        var alertTextField: UITextField!
+        alertController.addTextField { textField in
+            alertTextField = textField
+            textField.placeholder = "Task Name"
+        }
+        alertController.addAction(UIAlertAction(title: "Add", style: .default) { _ in
+            guard let text = alertTextField.text , !text.isEmpty else { return }
+
+            self.items.append(Task(value: ["text": text]))
+            self.tableView.reloadData()
+        })
+        present(alertController, animated: true, completion: nil)
+    }
+
+    // MARK: UITableView
+
+    override func tableView(_ tableView: UITableView?, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.text
+        cell.textLabel?.alpha = item.completed ? 0.5 : 1
+        return cell
     }
 }
 
@@ -27,7 +62,7 @@ final class TaskList: Object {
     dynamic var text = ""
     dynamic var id = ""
     let items = List<Task>()
-    
+
     override static func primaryKey() -> String? {
         return "id"
     }
