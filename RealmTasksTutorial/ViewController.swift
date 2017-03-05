b1eca7d117c30d4de100a607f01a7857bf8598e3
naoyamuto <naoya.mutou@gmail.com>
2017-03-06 00:03:43 +0900

    Add .gitignore

--- RealmTasksTutorial/ViewController.swift
+++ RealmTasksTutorial/ViewController.swift
@@ -7,6 +7,33 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    func setupUI() {
+        title = "My Tasks"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+}
+
+// MARK: Model
+
+final class TaskList: Object {
+    dynamic var text = ""
+    dynamic var id = ""
+    let items = List<Task>()
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+final class Task: Object {
+    dynamic var text = ""
+    dynamic var completed = false
 }
